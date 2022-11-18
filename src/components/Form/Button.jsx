9dfa09b5d1090d576663c2ba20e92fb4d21da34a
Hibinooo <rivo.ru12@gmail.com>
2022-11-18 23:27:24 +0500

    первый деплой

--- src/components/Form/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import style from './Form.module.scss'
-
-const Button = () => {
-  return (
-      <button className={style.button} name='Login'>Login</button>
-  )
-}
-export default Button
--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import timeReducer from "./timeSlice"
-export default configureStore({
-  reducer: {
-    times: timeReducer,
-  }
-})
